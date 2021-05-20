7a1951d9239c94574698c74be65e516acb6545cd
tanaybhardwaj24 <70639071+tanaybhardwaj24@users.noreply.github.com>
2021-05-20 12:18:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const fetchPokemons = async () => {
 };
 
 const getPokemon = async id => {
-    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}/`;
     const res = await fetch(url);
     const pokemon = await res.json();
     createPokemonCard(pokemon);
